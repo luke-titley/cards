c70ab23ed118144aa9c4a2096b0ac25f0707d1d1
Luke Titley <from+badja@luketitley.com>
2022-02-24 01:24:51 +0100

    Started code to make the svg

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,38 @@
 
-fn make_card(number : &u32, pairs : &Vec<(u32, u32)>)
+fn make_card(index : usize, number : &u32, pairs : &Vec<(u32, u32)>)
 {
+    if index % 2 == 0 {
+        print!("*");
+    }
     print!("{} -> ", number);
     for (a,b) in pairs {
         print!("[{0} {1}] ", a, b);
     }
     println!();
+
+    use svg::Document;
+    use svg::node::element::Path;
+    use svg::node::element::path::Data;
+
+    let data = Data::new()
+        .move_to((10, 10))
+        .line_by((0, 50))
+        .line_by((50, 0))
+        .line_by((0, -50))
+        .close();
+
+    let path = Path::new()
+        .set("fill", "none")
+        .set("stroke", "black")
+        .set("stroke-width", 3)
+        .set("d", data);
+
+    let document = Document::new()
+        .set("viewBox", (0, 0, 70, 70))
+        .add(path);
+
+    let file_path = format!("card_{:02}.svg", index);
+    svg::save(&file_path, &document).unwrap();
 }
 
 fn main() {
@@ -30,8 +57,8 @@ fn main() {
 
     flat.sort();
 
-    for (k, p) in flat.iter() {
-        make_card(k, &p);
+    for (i, (k, p)) in flat.iter().enumerate() {
+        make_card(i, k, &p);
     }
 
     println!("length = {}", flat.len());
